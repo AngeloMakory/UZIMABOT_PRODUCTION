af32d53f416f445bac0047c43d11b8670d6a71f2
AngeloMakory <angelomakory@gmail.com>
2024-02-08 20:34:16 +0300

    import re

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS
 import requests
 import os
+import re
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
